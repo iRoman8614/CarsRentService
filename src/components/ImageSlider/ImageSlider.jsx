f9ca3414fd23c76b08a007e0a821f5215fdbf832
Roman Baranovskii <rom8614@gmail.com>
2024-06-18 12:25:45 +0300

    ref

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/autoplay';
+
 import styles from './ImageSlider.module.css';
 
 const ImageSlider = ({ images }) => {
